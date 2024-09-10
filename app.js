08e3069e43e419c013513be5a69723e1f4315ba9
ArauzB <arauz220678@gmail.com>
2024-09-10 00:49:05 -0600

    agree headers

--- app.js
+++ app.js
@@ -172,6 +172,11 @@ async function playNextSong(guildId, message) {
     quality: "highestaudio",
     filter: "audioonly", 
     highWaterMark: 1 << 25, 
+    requestOptions: {
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3', // Cambia el User-Agent para parecer una solicitud desde un navegador
+      }
+    }
   };
 
   const stream = ytdl(song.url, streamOptions);
